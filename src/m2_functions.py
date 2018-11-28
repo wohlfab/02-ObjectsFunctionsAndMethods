5aebe8f74098a5d67f6cd23495e90211fd9a99a0
wohlfab <wohlfab@rose-hulman.edu>
2018-11-28 14:29:20 -0500

    Classwork 1

--- src/m2_functions.py
+++ src/m2_functions.py
@@ -84,7 +84,7 @@ def make(color, s):
 
 
 ###############################################################################
-# TODO: 5.
+# DONE: 5.
 #   COMMIT-and-PUSH your work (after changing this TO-DO to DONE).
 #
 #   As a reminder, here is how you should do so:
